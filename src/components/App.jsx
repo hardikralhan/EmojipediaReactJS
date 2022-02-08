230fcc75bf4485ff348737261878ccacd2b77f33
Hardik <hardikralhan66@gmail.com>
2022-02-08 14:39:37 +0530

    Function appearance changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import Entry from "./Entry";
 import Emojipedia from "../emojipedia";
 
-function createList(emojipedia) {
-  return (
-    <Entry
-      key={emojipedia.id}
-      name={emojipedia.name}
-      emoji={emojipedia.emoji}
-      meaning={emojipedia.meaning}
-    />
-  );
-}
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{Emojipedia.map(createList)}</dl>
+      <dl className="dictionary">
+        {Emojipedia.map((emojipedia) => {
+          return (
+            <Entry
+              key={emojipedia.id}
+              name={emojipedia.name}
+              emoji={emojipedia.emoji}
+              meaning={emojipedia.meaning}
+            />
+          );
+        })}
+      </dl>
     </div>
   );
 }
